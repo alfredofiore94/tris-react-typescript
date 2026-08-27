08b91907a9948e635353dfaff3079b847e2e6553
Alfredo Fiore <afiore@florence-consulting.it>
2026-08-27 11:24:14 +0200

    aggiunta rendering condizionale per modifica nome e buttons confirm e cancel

--- src/components/player.tsx
+++ src/components/player.tsx
@@ -2,15 +2,47 @@ import { useState } from "react";
 import type { Player } from "../models/player";
 
 export function Player(player: Player) {
-  const [namePlayer, setNamePlayer] = useState<string>("");
-  function OnClickEditName(namePlayer: string) {
-    setNamePlayer(namePlayer);
+  const [namePlayer, setNamePlayer] = useState<string>(player.name);
+  const [isEditingName, setIsEditingName] = useState<boolean>(false);
+
+  function EditName(nameEdit: string) {
+    setNamePlayer(nameEdit);
   }
   return (
     <li>
-      <span className="player-name"> {player.name}</span>
-      <span className="player-symbol"> {player.symbol}</span>
-      <button>Modifica nome giocatore</button>
+      <span className="player">
+        {!isEditingName ? (
+          <>
+            <span className="player-name"> {namePlayer}</span>
+            <span className="player-symbol"> {player.symbol}</span>
+            <button
+              onClick={() => {
+                setIsEditingName(true);
+              }}
+            >
+              Modifica nome giocatore
+            </button>{" "}
+          </>
+        ) : (
+          <>
+            <input type="text" required value={player.name} />
+            <button
+              onClick={() => {
+                setIsEditingName(false);
+              }}
+            >
+              ✅
+            </button>
+            <button
+              onClick={() => {
+                setIsEditingName(false);
+              }}
+            >
+              ❌
+            </button>
+          </>
+        )}
+      </span>
     </li>
   );
 }
